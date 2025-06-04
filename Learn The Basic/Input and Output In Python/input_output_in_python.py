33ee603c171cbe242624bb901bd7299375d6a2bb
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-04 11:58:47 +0530

    added input_output_problem for striver dsa a2z sheet

---
+++ Learn The Basic/Input and Output In Python/input_output_in_python.py
@@ -0,0 +1,19 @@
+
+#User function Template for python3
+########### Write your code below ###############
+
+# Take string input and print the string input
+
+# Take integer input and add 10 to the integer input and print
+
+# Take floating-point input and multiply the float input by 10 and print
+
+########### Write your code above ###############
+s=input()
+i=10+int(input())
+f=10*float(input())
+print(s)
+print(i)
+print(f)
+
+
